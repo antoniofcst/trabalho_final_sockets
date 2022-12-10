4ff725ca9fd6bd0070792da8e3f8441176464e59
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-12-10 08:11:39 -0300

    Estabelecimento da conexão

--- client.py
+++ client.py
@@ -1,1 +1,9 @@
 client.py
+
+import socket #biblioteca padrão no python 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #definindo um objeto socket 
+s.connect((socket.gethostname(), 1234))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,1 +1,13 @@
 server.py
+
+import socket #biblioteca padrão no python 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #definindo um objeto socket 
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+	clientsocket, address = s.accept()
+	print(f"Conexão a partir de {address} foi estabelecida!")
+	clientsocket.send(bytes("Bem-vindos ao server!", "utf-8"))
+	
