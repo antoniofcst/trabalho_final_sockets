5638cd650e16f41ab2a0fa718beccbec92d340cb
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-12-10 08:46:38 -0300

    Conectando o servidor e corrigindo bugs

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 server.py
 
 import socket #biblioteca padrão no python 
-
+# criar um soquete INET, STREAMing
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #definindo um objeto socket 
+# vincularndo o socket a um host 
 s.bind((socket.gethostname(), 1234))
+# tornando um socket de servidor
 s.listen(5)
 
 while True:
 	clientsocket, address = s.accept()
 	print(f"Conexão a partir de {address} foi estabelecida!")
 	clientsocket.send(bytes("Bem-vindos ao server!", "utf-8"))
-	
