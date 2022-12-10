fb19b3862f233ea5d08c152331fb2f008a97b4fd
Eduardo de Oliveira Sousa <84216150+eduardo-559@users.noreply.github.com>
2022-12-10 16:40:39 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-server.py
+import socket
+import select
 
-import socket #biblioteca padrão no python 
-import select 
-# criando um socket 
-HEADER_LENGTH = 10
+HEADER_LENGHT = 10
 IP = "127.0.0.1"
 PORT = 1234
 
@@ -11,81 +9,59 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server_socket.bind((IP, PORT))
-
 server_socket.listen()
 
-sockets_list = [server_socket]
+socket_list = [server_socket]
 
-clients = {}
+client = {}
 
-# recebendo mensagem 
+def receive_message(cliente_socket):
+    try:
+        message_header = cliente_socket.recv(HEADER_LENGHT)
 
-def receive_message(client_socket):
-	try:
-		message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):
+            return False
 
-		if not len(message_header):
-			return False 
-
-		message_length = int(message_header.decode("utf-8").strip())
-
-		return {"header": message_header, "data": client_socket.recv(message_length)}
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": cliente_socket.recv(message_length)}
 
 
-	except:
-		return False 
+    except:
+        return False
 
-while True: 
-	read_sockets, __, exception_sockets = select.select(socket_list, [], sockets_list)
-	# lendo sockets 
+while True:
+    read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
 
-	for notified_socket in read_sockets:
-		if notified_socket == server_socket:
-			client_socket, client_address = server_socket.accept()
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
 
-			user = receive_message(client_socket)
-			if user is False: 
-				continue 
+            user = receive_message(client_socket)
+            if user is False:
+                continue
 
-			sockets_list.append(client_socket)
+            socket_list.append(client_socket)
 
-			clients[client_socket] = user 
+            client[client_socket] = user
 
-			print(f"Nova conexão aceita a partir da {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
-		else:
-			message = receive_message(notified_socket)
+            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
 
-			if message is False:    
-				print(f"Conexão encerrada a partir da {clients[notified_socket]['data'].decode('utf-8')}")
-				sockets_list.remove(notified_socket)
-				del clients[notified_socket]
-				continue
+        else:
+            message = receive_message(notified_socket)
 
-			user = clients[notified_socket]
-			print(f"Recebendo mensagem a partir de {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+            if message is False:
+                print(f"Closed connection from {client[notified_socket]['data'].decode('utf-8')}")
+                socket_list.remove(notified_socket)
+                del client[notified_socket]
+                continue
 
+            user = client[notified_socket]
+            print(f"Received message from {user['data'].decode['utf-8']}: {message['data'].decode['utf-8']}")
 
+            for client_socket in client:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
-			for client_socket in clients:
-				if client_socket != notified_socket:
-					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
-
-	for notified_socket in exception_sockets:
-		sockets_list.remove(notified_socket)
-		del clients[notified_socket]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for notified_socket in exception_sockets:
+        socket_list.remove(notified_socket)
+        del client[notified_socket]
