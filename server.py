73b3698ffe61527b4a09b551bef1c2ddd52b10a6
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-12-10 10:10:15 -0300

    Lendo sockets

--- server.py
+++ server.py
@@ -1,14 +1,63 @@
 server.py
 
 import socket #biblioteca padrão no python 
-# criar um soquete INET, STREAMing
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #definindo um objeto socket 
-# vincularndo o socket a um host 
-s.bind((socket.gethostname(), 1234))
-# tornando um socket de servidor
-s.listen(5)
+import select 
+# criando um socket 
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
 
-while True:
-	clientsocket, address = s.accept()
-	print(f"Conexão a partir de {address} foi estabelecida!")
-	clientsocket.send(bytes("Bem-vindos ao server!", "utf-8"))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+# recebendo mensagem 
+
+def receive_message(client_socket):
+	try:
+		message_header = client_socket.recv(HEADER_LENGTH)
+
+		if not len(message_header):
+			return False 
+
+		message_length = int(message_header.decode("utf-8").strip())
+
+		return {"header": message_header, "data": client_socket.recv(message_length)}
+
+
+	except:
+		return False 
+
+while True: 
+	read_sockets, __, exception_sockets = select.select(socket_list, [], sockets_list)
+	# lendo sockets 
+
+	for notified_socket in read_sockets:
+		if notified_socket == server_socket:
+			client_socket, client_address = server_socket.accept()
+
+			user = receive_message(client_socket)
+			if user is False: 
+				continue 
+
+			sockets_list.append(client_socket)
+
+			clients[client_socket] = user 
+
+			print(f"Nova conexão aceita a partir da {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+
+
+
+
+
+
+
+
